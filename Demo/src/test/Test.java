c9ece83a6b7b3c0f908a90a8804a3f00ac1eb052
sharadsable <swatissable@gmail.com>
2022-03-30 14:44:31 +0530

    changes by y

--- Demo/src/test/Test.java
+++ Demo/src/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s="hello";
-		System.out.println("hello");
+		System.out.println("hello al");
 	}
 
 }
