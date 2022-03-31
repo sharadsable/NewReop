2d359b4c6feaf170f791f8fae81cdc98748526c0
sharadsable <swatissable@gmail.com>
2022-03-31 11:53:52 +0530

    changes by devx

--- Demo/src/develop/developapp.java
+++ Demo/src/develop/developapp.java
@@ -1,11 +1,10 @@
-package test;
+package develop;
 
-public class test1 {
+public class developapp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String s="hello";
-System.out.println("done");
+		System.out.println("done");
 	}
 
 }
